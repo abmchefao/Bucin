5cbe40ab54a74e66f4aa5ed072acf21040d32eba
⋆✰٧⋅ꍏ.ℬ.Ꮇ⋄ChefÃ£o♔✰⋆ <47883842+abmchefao@users.noreply.github.com>
2020-02-05 22:31:31 +0700

    Add files via upload

---
+++ Bucin.sh
@@ -0,0 +1,116 @@
+#Coded by ABMchefao
+#Recode aja ya bangsat!!
+#skuy ngewe awkoawokoawokwo
+
+
+green="\033[32;1m"
+yellow="\033[33;1m"
+indigo="\033[34;1m"
+red="\033[35;1m"
+purple="\033[37;1m"
+cyan="\033[36;1m"
+white="\033[39;1m"
+
+clear
+echo $indigo"==================================================================="
+ 
+echo $indigo"*" $red"AKU HEKER: ABMchefao"
+echo $indigo"*" $yellow"Team : Funny Cyver Team"
+echo $indigo"*" $white"Contact : 089-535-908-8252"
+
+echo $indigo"==================================================================="
+echo
+
+echo $cyan "1. Bucin by. ABMchefao(afnanbagus) "
+
+echo $red "2. Bucin by. ABMchefao(afnanbagus) 2"
+
+echo
+echo $indigo"╭─Pilih Mau Bucin Siapa??
+"read -p "╰─•> " pil
+
+if [ $pil = '1' ]
+then
+echo
+echo
+clear
+echo
+toilet -f future Bucin by ABMchefao | lolcat
+echo
+echo
+echo
+sleep 0.1
+echo $green"Kamu tau gk arti dari rindu"
+sleep 2
+echo $red"Wassek aowkwowko ndak pantes aku ndadi bucin? fakboy wes aowkwowkk "
+sleep 2
+echo $green"Lanjut ya? oky oky :)"
+sleep 2
+echo $red"Arti dari rindu adalah arti di mana kita harus saling mengerti"
+sleep 2
+echo $green"Dan saling mempercayai satu sama lain"
+sleep 2
+echo $red"ketika itu di mana kita ingin sesekali bertemu dan menghampirinya"
+sleep 2
+echo $green"berharap untuk bisa memilikinya secara lebih dan bisa selalu menemuinya"
+sleep 2
+echo $red"di situlah arti dari kerinduan"
+sleep 2
+echo $red"Ecie baper aowkwokwo ngunu tok wae baper aso aso V:"
+sleep 3
+clear
+echofiglet -f future Bucin by ABMchfao | lolcat
+echo
+echo
+echo
+sleep 0.1
+echo $red"Baper nih?"
+echo
+echo
+sleep 2
+echo $cyan"Ya pasti lah afnan kan kamg fak boy"
+sleep 2
+echo $red"wkwkwkw canda kok"
+sleep 2
+echo $cyan"Moga harimu menyenangkan"
+sleep 1
+echo $red"Seeyou"
+sleep 2
+echo $cyan"
+hati hati syang...."
+sleep 2
+echo $red"haha.."
+sleep 2
+echo $cyan"I love you"
+sleep 2
+echo $red"Need you slalu"
+sleep 3
+fi
+
+if [ $pil = '2' ]
+then
+clear
+echo
+toilet -f future Bucin by ABMchefao 2 | lolcat
+echo
+echo
+sleep 0.1
+echo $green"Sek sek tak mikir V:"
+sleep 2
+echo $red"ngene wae wes aowowkowko"
+sleep 2
+echo $green"Aku to sayang kue ndah jnck"
+sleep 2
+echo $red"Kue reti porak to aso"
+sleep 2
+echo $green"asw ndes pingin tak pancal raimu"
+sleep 2
+echo $red"awokwowkwokow"
+sleep 2
+echo $red"Ecie Gk jelas ye emang njer aowkwowko"
+sleep 3
+fi
+
+
+#thnks ya klau udh gunain tools gblk saya
+#moga tambah gblk awoekoekeo
